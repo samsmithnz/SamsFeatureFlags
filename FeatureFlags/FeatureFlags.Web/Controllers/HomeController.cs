e140a51810da4d68b844d98619ca4ee09df0061a
Sam Smith <samsmithnz@gmail.com>
2020-05-13 09:15:10 -0400

    Trying to force the checks to run again

--- FeatureFlags/FeatureFlags.Web/Controllers/HomeController.cs
+++ FeatureFlags/FeatureFlags.Web/Controllers/HomeController.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using FeatureFlags.Models;
+using FeatureFlags.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using FeatureFlags.Web.Models;
-using FeatureFlags.Models;
-using Microsoft.Extensions.FileProviders;
 
 namespace FeatureFlags.Web.Controllers
 {
