87af50163e8e5b97a292d64cd47918be65041178
Sam Smith <samsmithnz@gmail.com>
2021-08-02 09:37:05 -0400

    FIxed demo project to be MVC

--- FeatureFlags/FeatureFlagsDemo.Web/Startup.cs
+++ FeatureFlags/FeatureFlagsDemo.Web/Startup.cs
@@ -23,7 +23,7 @@ namespace FeatureFlagsDemo.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,11 +35,10 @@ namespace FeatureFlagsDemo.Web
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -49,7 +48,9 @@ namespace FeatureFlagsDemo.Web
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
