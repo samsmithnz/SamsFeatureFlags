6543db31635b687678eec1d469bdd25410617bdb
Sam Smith <samsmithnz@gmail.com>
2022-06-26 15:34:19 -0400

    Experimental Blazor version of website (#578)
    * Creating Blazor version
    
    * Added Blazor template
    
    * Updated solution
    
    * Added app insights and app settings
    
    * Added PR to feature index in blazor
    
    * Got toggle to work
    
    * fixed error mesesage
    
    * Saving working!
    
    * Added null result

---
+++ FeatureFlags/FeatureFlags.Web2/Data/Models/Payload.cs
@@ -0,0 +1,16 @@
+﻿namespace FeatureFlags.Web2.Data.Models
+{
+    public class Payload<T>
+    {
+        public Payload()
+        {
+            ServiceMessage = "";
+            ServiceError = "";
+        }
+
+        public T Data { get; set; }
+        public string ServiceURL { get; set; }
+        public string ServiceMessage { get; set; }
+        public string ServiceError { get; set; }
+    }
+}
---
+++ FeatureFlags/FeatureFlags.Web2/Program.cs
@@ -0,0 +1,33 @@
+using FeatureFlags.Web2.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<IServiceAPIClient, ServiceAPIClient>();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
